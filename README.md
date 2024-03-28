0251bb482ac94d64da61b9d5d73ec9f76e586186
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-03-28 13:36:52 +0545

    swornim changes

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 👉synchronous-the code runs in a particular sequences of instruction given in the program.Each instruction waits fot the previous instruction to complete the execution
 👉Asynchronous;Due to synchronous programming , sometimes impinstruction get blocked due to some previous instructions, which causes a delay in the ul o async code execution allows to execute nextinstruction immediately and doesnot block the flow
 👉Callback:any function that is passed in a another function as an argument is called callback function
-settumeout also is an example of callback 
+settumeout also is an example of callback
 👉callback Hell🔥:Nested callback stacjed below one another forming a pyramid stucture,
 noteL donot use paranthesis in callback function use instead arrow function
-👉Promises: to solve the callback hell🔥 problem
+👉Promises: to solve the callback hell🔥 problem <unfullfilled resolve reject>
+is a object in js
+new promises((resolve, reject)=>{}) this reolve and reject are callback fun in js
+Promises State🫴🏻
+Pending
+Fullfilled[resolved]
+Rejected
+
+promise.then((res)=>{...}) if fullfilled and we want to work anything after fullfill
+promise.catch((err)=>{...}) if fullfilled and we want to work anything after fullfill
+👉🏻Promises Chaining
--- app.js
+++ app.js
@@ -24,7 +24,7 @@
 //         if (getNextData) {
 //             getNextData()
 //         }
-        
+
 //     },2000)
 // }
 
@@ -33,20 +33,75 @@
 //         getData(3)
 //     })
 // })
-function getData(dataId, getNextData){
-    setTimeout(()=>{
-        console.log("data", dataId);
-        if (getNextData) {
-            getNextData()    
-        }
-        
-    },2000)
-}
-//callback hell🔥🔥
-getData(1, ()=>{
-    getData(2,  ()=>{
-        getData(3)
-    
-    })
+// function getData(dataId, getNextData) {
+//   setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);
+// }
+// //callback hell🔥🔥
+// getData(1, () => {
+//   getData(2, () => {
+//     getData(3, () => {
+//       getData(4);
+//     });
+//   });
+// });
 
-})
+//promises
+// const getPromises = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("I am a promise");
+//     // resolve("success");
+//     reject("some error occur");
+//   });
+// };
+
+// let promise = getPromises();
+// promise.then(() => {
+//   console.log("Promises fullfilled");
+// });
+// promise.catch(() => {
+//   console.log("Error occur");
+// });
+
+// function getData(dataId, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       resolve("sucess");
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 5000);
+//   });
+// }
+
+const asyncFunc1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data1");
+      resolve("success");
+    }, 4000);
+  });
+};
+
+const asyncFunc2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data2");
+      resolve("success");
+    }, 4000);
+  });
+};
+
+console.log("Fetching Data 1");
+asyncFunc1().then((res) => {
+  console.log(res);
+  console.log("Fetching Data 2");
+  asyncFunc2().then((res) => {
+    console.log(res);
+  });
+});
